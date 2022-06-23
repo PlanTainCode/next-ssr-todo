0d04605425f2488351ac03e70ae7682ce328a6fe
PlanTainCode <plancode14@gmail.com>
2022-06-23 15:03:05 +0300

    main

--- README.md
+++ README.md
@@ -1,34 +1,25 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# NEXT.JS SSR TO-DO APPLICATION
 
-## Getting Started
+Простое Server Side Rendering приложение написанное, с помощью Next.js. 
 
-First, run the development server:
+В качестве базы данных используется MongoDB
 
-```bash
+## Установка:
+
+Чтобы установить приложение необходимо после скачивания репозитория выполнить команду:
+
+```sh
+cd next-todo
+npm i
 npm run dev
-# or
-yarn dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## База данных:
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+для полноценной работы приложения необходимо создать `.env.local` и добавить туда:
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+```sh
+DB_URL=Ссылка для подключения к базе данных
+```
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+### Приятного использования !
---
+++ pages/index.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import axios from 'axios'
+import { Checkbox, IconButton, Paper, Typography } from '@mui/material'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+import { EditOutlined as Edit, DeleteOutlineOutlined as Delete, PlaylistAddCheck as Icon  } from '@mui/icons-material';
+
+// const url = process.env.SERVER_URL;
+const url = "http://localhost:3000/api/task";
+
+export default function Home(props) {
+  const [tasks, setTasks] = React.useState(props.tasks);
+  const [task, setTask] = React.useState({task: ""});
+
+  const handleChange = ({currentTarget: input}) => {
+    input.value === ""
+    ? setTask({task: ""})
+    : setTask((prev) => ({...prev, task: input.value}));
+  }
+
+  const editTask = (id) => {
+    const currentTask = tasks.filter((task) => task._id === id);
+    setTask(currentTask[0]);
+  }
+
+  const addTask = async (e) => {
+    e.preventDefault();
+    try {
+      if (task._id) {
+        const {data} = await axios.put(url + "/" + task._id, {task: task.task});
+        const originalTasks = [...tasks];
+        const index = originalTasks.findIndex((t) => t._id === task._id);
+        originalTasks[index] = data.data;
+        setTasks(originalTasks);
+        setTask({task: ""});
+        console.log(data.message);
+      } else {
+        const {data} = await axios.post(url, task);
+        setTasks((prev) => [...prev, data.data]);
+        setTask({task: ""});
+        console.log(data.message)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const updateTask = async (id) => {
+		try {
+			const originalTasks = [...tasks];
+			const index = originalTasks.findIndex((t) => t._id === id);
+			const { data } = await axios.put(url + "/" + id, {
+				complited: !originalTasks[index].complited,
+			});
+			originalTasks[index] = data.data;
+			setTasks(originalTasks);
+			console.log(data.message);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+  const deleteTask = async (id) => {
+    try {
+      const {data} = await axios.delete(url + "/" + id);
+      setTasks((prev) => prev.filter((task) => task._id !== id));
+      console.log(data.message);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <Typography variant='h1'><Icon /> PlanTain To-Do</Typography>
+        <span></span>
+      </header>
+      <main className={styles.main}>
+        <Paper elevation={1} className={styles.formPaper}>
+          <form onSubmit={addTask} className={styles.form}>
+            <input type="text" placeholder='Впишите задачу' value={task.task} onChange={handleChange} />
+            <button type='submit'>{task._id ? "Обновить" : "Добавить"}</button>
+          </form>
+        </Paper>
+        <Paper elevation={1} className={styles.taskBlock}>
+          {tasks.map((task) => (
+             <div className={styles.taskItem} key={task._id}>
+              <div className={styles.titleTask}>
+                <Checkbox checked={task.complited} onChange={() => updateTask(task._id)} />
+                <Typography variant='inherit' className={task.complited ? styles.lineThrow : ""}>{task.task}</Typography>
+              </div>
+              <div className={styles.buttonsTask}>
+                <IconButton onClick={() => editTask(task._id)}><Edit /></IconButton>
+                <IconButton onClick={() => deleteTask(task._id)}><Delete /></IconButton>
+              </div>
+            </div>
+          ))}
+          {tasks.length === 0 && <Typography variant='h2'>Пока нет задач</Typography>}
+        </Paper>
+      </main>
+    </div>
+  )
+}
+
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(url);
+  return {
+    props: {
+      tasks: data.data
+    }
+  }
+}
