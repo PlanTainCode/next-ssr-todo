4a6756a444bca9910a483b17086d6f4b161bb69e
PlanTainCode <plancode14@gmail.com>
2022-06-23 17:11:36 +0300

    main

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        source: "/api/task",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
